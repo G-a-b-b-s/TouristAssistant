02235cea06a0a4313326292cab64e713a8fe4af1
Paweł Froń <pavelfron4@gmail.com>
2024-11-12 15:59:19 +0100

    algorithm

--- Pathfinding/Graph.py
+++ Pathfinding/Graph.py
@@ -1,5 +1,6 @@
 import math
 from typing import Self, List, Set
+from copy import deepcopy
 
 from PointOfInterest import POI
 
@@ -12,40 +13,91 @@ def distance(v1: POI, v2: POI):
     return R * math.sqrt(cmp)
 
 class Graph:
-    def __init__(self, vertices: List[POI]):
+    def __init__(self, vertices: List[POI], matrix: List[List[float]] = None):
         self.vertices = vertices
-        self.matrix = [[distance(v1, v2) for v2 in vertices] for v1 in vertices]
+
+        if matrix:
+            self.matrix = matrix
+        else:
+            self.matrix = [[distance(v1, v2)
+                        for v2 in vertices]
+                        for v1 in vertices]
 
     def MST(self) -> Self:
-        mst = Graph(self.vertices)
-        num_of_vertices = len(self.vertices)
+        n = len(self.vertices)
+        mst = [[0 for _ in self.vertices] for _ in self.vertices]
 
-        covered: Set[int] = {0}
-        not_covered: Set[int] = set([x for x in range(1, num_of_vertices)])
-        edges: Set[(int, int)] = set()
+        all_vertices= set(range(n))
+        covered = {0}
 
-        while not_covered:
-            min: float = float('inf')
-            selected_vertix: int
-            previous_vertix: int
+        while len(covered) != n:
+            min = float('inf')
+            next_vertix = 0
+            previous_vertix = 0
 
-            for i in covered:
-                for j in not_covered:
-                    d = self.matrix[i][j]
-                    if d <= min:
-                        min = d
-                        selected_vertix = j
-                        previous_vertix = i
+            for v1 in covered:
+                for v2 in all_vertices - covered:
+                    distance = self.matrix[v1][v2]
+                    if distance <= min:
+                        min = distance
+                        next_vertix = v2
+                        previous_vertix = v1
 
-            not_covered.remove(selected_vertix)
-            covered.add(selected_vertix)
-            edges.add((selected_vertix, previous_vertix))
+            covered.add(next_vertix)
+            mst[next_vertix][previous_vertix] = min
+            mst[previous_vertix][next_vertix] = min
+
+        return Graph(self.vertices, mst)
+    
+    def path_distance(self, path):
+        distance = 0
+        for i in range(len(path) - 1):
+            distance += self.matrix[path[i]][path[i + 1]]
+        return distance
+
+    def find_path(self, start: int) -> List[POI]:
+        n = len(self.matrix)
+        edges = deepcopy(self.matrix)
+        path = []
+
+        def DFS(v1):
+            for v2 in range(n):
+                if edges[v1][v2] != 0:
+                    edges[v1][v2] = 0
+                    DFS(v2)
+
+            if set(range(n)) - set(path):
+                path.append(v1)
+
+        DFS(start)
+        return [self.vertices[v] for v in path]
+
+    def optimize_path(self, initial_path: List[POI]) -> List[POI]:
+        best_path = [self.vertices.index(x) for x in initial_path]
+        best_distance = self.path_distance(best_path)
+        improved = True
+
+        while improved:
+            improved = False
+            for i in range(1, len(best_path) - 2):
+                for j in range(i + 1, len(best_path) - 1):
+                    new_path = best_path[:i] + best_path[i:j+1][::-1] + best_path[j+1:]
+                    new_distance = self.path_distance(new_path)
+
+                    if new_distance < best_distance:
+                        best_path = new_path
+                        best_distance = new_distance
+                        improved = True
+                        break
+                if improved:
+                    break
         
-        for i in range(len(self.vertices)):
-            for j in range(len(self.vertices)):
-                if (i, j) not in edges and (j, i) not in edges: 
-                    mst.matrix[i][j] = 0
-        return mst
+        result = [best_path[0]]
+        for i in range(1, len(best_path)):
+            if best_path[i] != result[-1]:
+                result.append(best_path[i])
+
+        return [self.vertices[v] for v in result]
 
 pois = [
     POI('first', 'castle', 37.7749, -122.4194, {}),
@@ -55,4 +107,7 @@ pois = [
     POI('fifth', 'idk', 37.7750, -122.4202, {})
 ]
 graph = Graph(pois)
-graph.MST()
+mst = graph.MST()
+initial_path = mst.find_path(0)
+best_path = mst.optimize_path(initial_path)
+print(best_path)
--- Pathfinding/PointOfInterest.py
+++ Pathfinding/PointOfInterest.py
@@ -5,3 +5,14 @@ class POI:
         self.latitude = latitude
         self.longitude = longitude
         self.tags = tags
+    
+    def __eq__(self, value):
+        if isinstance(value, POI):
+            return (self.name, self.type, self.latitude, self.longitude) == (value.name, value.type, value.latitude, value.longitude)
+        return False
+
+    def __hash__(self):
+        return hash((self.name, self.type, self.latitude, self.longitude))
+
+    def __repr__(self):
+        return f"{self.name}, {self.type} ({self.latitude}, {self.longitude})"
